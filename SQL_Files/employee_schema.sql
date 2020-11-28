51b8815bb6ddca1ef67a90eb5b76f0e7e1e96335
chris-inalsingh <69774703+chris-inalsingh@users.noreply.github.com>
2020-11-28 09:25:43 -0500

    Update employee_schema.sql

--- SQL_Files/employee_schema.sql
+++ SQL_Files/employee_schema.sql
@@ -1,13 +1,21 @@
+-- Create titles table
+Create Table titles (
+	title_id VARCHAR(200) PRIMARY KEY NOT NULL,
+	title VARCHAR(200) NOT NULL
+);
+
 --Create employees table
 Create Table employees(
 	emp_no INT PRIMARY KEY NOT NULL,
-	emp_title VARCHAR(200) UNIQUE NOT NULL,
+	emp_title VARCHAR(200) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(200) NOT NULL,
 	last_name VARCHAR(200) NOT NULL,
 	sex VARCHAR(200)NOT NULL,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
+	foreign key (emp_title) references titles(title_id)
 );
+
 --Create departments table
 Create Table departments(
 	dept_no VARCHAR(200) PRIMARY KEY NOT NULL,
@@ -16,7 +24,7 @@ Create Table departments(
 
 --Create dept_emp table
 Create Table dept_emp(
-	emp_no INT PRIMARY KEY NOT NULL,
+	emp_no INT NOT NULL,
 	dept_no VARCHAR(200) NOT NULL,
 	foreign key (emp_no) references employees(emp_no),
 	foreign key (dept_no) references departments(dept_no)
@@ -24,17 +32,10 @@ Create Table dept_emp(
 
 --Create dept_manager table
 Create Table dept_manager (
-	dept_no VARCHAR(200) PRIMARY KEY NOT NULL,
-	emp_no INT NOT NULL,
+	dept_no VARCHAR(200) NOT NULL,
+	emp_no INT PRIMARY KEY NOT NULL,
     foreign key (emp_no) references employees (emp_no),
     foreign key (dept_no) references departments (dept_no)
-);
-
--- Create titles table
-Create Table titles (
-	title_id VARCHAR(200) PRIMARY KEY NOT NULL,
-	title VARCHAR(200) NOT NULL,
-    foreign key (title_id) references employees(emp_title)
 );
 
 --Create salaries table
